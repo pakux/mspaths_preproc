5917c78af2e69cfeb6f91fdce22a557336e4e742
Paul Kuntke <paul.kuntke@tu-dresden.de>
2025-08-11 12:53:30 +0100

    Nipype-Node for hdbet

--- nipype_hdbet.py
+++ nipype_hdbet.py
@@ -6,10 +6,9 @@ from nipype.interfaces.base.core import BaseInterface, BaseInterfaceInputSpec
 from nipype.interfaces.base import traits, TraitedSpec
 from HD_BET.checkpoint_download import maybe_download_parameters
 from HD_BET.hd_bet_prediction import get_hdbet_predictor, hdbet_predict
-from os.path import basename
-from nipype
+from os.path import basename, abspath
 
-
+""" 
 inputs:
 
     in_file 
@@ -25,15 +24,16 @@ run:
 
     maybe_download_parameters()
     predictor = get_hdbet_predictor(
-            )
+            ) 
+            
+"""
 
 class HD_BET_BrainextractorInputSpec(BaseInterfaceInputSpec):
     in_file = traits.File(exists=True, mandatory=True)
 
 class HD_BET_BrainextractorOutputSpec(TraitedSpec):
     out_file = traits.File(exists=True, mandatory=True)
-
-
+    out_mask = traits.File(exists=True, mandatory=True)
 
 class HD_BET_Brainextractor(BaseInterface):
     input_spec = HD_BET_BrainextractorInputSpec
@@ -46,18 +46,34 @@ class HD_BET_Brainextractor(BaseInterface):
         predictor = get_hdbet_predictor(
             )
 
-
-        output_filename = basename(self.inputs['in_file'] )
-
-        hdbet_predict(self.inputs['in_file'], output_filename)
+        output_filename =basename(self.inputs.in_file)
+        hdbet_predict(self.inputs.in_file, output_filename, predictor, keep_brain_mask=True)
 
 
-        self._results['out_file'] =
+       # self._results['out_file'] = abspath(output_filename)
+        
 
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['out_file'] = self._results['out_file']
+        output_filename =basename(self.inputs.in_file)
+        mask_filename = output_filename.replace('.nii', '_bet.nii')
+        
+        outputs['out_file'] = abspath(output_filename)
+        outputs['out_mask'] = abspath(mask_filename) 
         return outputs  
 
+
+
+
+def test_hdbet(in_file):
+
+    from nipype import Node
+    hdbet = Node(HD_BET_Brainextractor(), name='ndbet')
+    hdbet.inputs.in_file = in_file
+    results = hdbet.run()
+
+    print(results)
+
+    return results
